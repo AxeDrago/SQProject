b0f10ac05f5eaf2188f2956066f10e222a962f25
clifton26 <clifton.clunie26@gmail.com>
2016-05-30 20:15:42 +0100

    Web page structure Test

--- src/test/java/CliftonTest.java
+++ src/test/java/CliftonTest.java
@@ -16,13 +16,15 @@ public class CliftonTest {
     private boolean acceptNextAlert = true;
     private static StringBuffer verificationErrors = new StringBuffer();
 
+
     @BeforeClass
     public static void setUp() throws Exception {
        //driver = new FirefoxDriver();
        driver = new HtmlUnitDriver();
-       baseUrl = "http://stagingserverqs.westeurope.cloudapp.azure.com/CluniePersonal.html";
-       //baseUrl = "http://127.0.0.1:8080/CluniePersonal.html";
+       //baseUrl = "http://stagingserverqs.westeurope.cloudapp.azure.com/CluniePersonal.html";
+       baseUrl = "http://127.0.0.1:8080/CluniePersonal.html";
        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 
     }
 
@@ -32,14 +34,51 @@ public class CliftonTest {
         Assert.assertEquals("Clifton Personal Page - SQ", driver.getTitle());
         try {
             Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "MEICM - STUDENT");
-        } catch (Error e) {
+            } catch (Error e) {
             verificationErrors.append(e.toString());
         }
         try {
             Assert.assertEquals(driver.findElement(By.xpath("//p[2]")).getText(), "Panama");
             } catch (Error e) {
                 verificationErrors.append(e.toString());
-            }
+        }
+
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h3")).getText(), "Name:");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h3[2]")).getText(), "Country:");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h3[3]")).getText(), "Address:");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h3[4]")).getText(), "Skills:");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h3[5]")).getText(), "Description:");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h3[6]")).getText(), "Social Media Link:");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            Assert.assertEquals(driver.findElement(By.xpath("//h2")).getText(), "Comments");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+
 
     }
 
