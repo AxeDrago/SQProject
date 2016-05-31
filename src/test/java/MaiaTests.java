49107102dcda23a28be43607dd490db4d2cb3686
Ruben <maia.ruben@gmail.com>
2016-05-31 17:25:44 +0100

    Main test class

--- src/test/java/MaiaTests.java
+++ src/test/java/MaiaTests.java
@@ -48,7 +48,7 @@ public class MaiaTests{
     }
 
     @Test
-    public void testEstruturaPersonal() throws Exception {
+    public void testPersonal() throws Exception {
         driver.get(baseUrl + "/MaiaPersonal.html");
         assertEquals("Rúben Personal Page - SQ", driver.getTitle());
         try {
@@ -73,6 +73,179 @@ public class MaiaTests{
         } catch (Error e) {
             verificationErrors.append(e.toString());
         }
+    }
+
+    @Test
+    public void testPersonalStructure() throws Exception {
+        driver.get(baseUrl + "/MaiaPersonal.html");
+        assertEquals("Name:", driver.findElement(By.cssSelector("h3")).getText());
+        try {
+            assertEquals("Name:", driver.findElement(By.cssSelector("h3")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Name:".equals(driver.findElement(By.cssSelector("h3")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Country:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[2]")).getText());
+        assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[2]")));
+        try {
+            assertEquals("Country:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[2]")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[2]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Country:".equals(driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[2]")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[3]"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Contacts:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[4]")).getText());
+        assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[4]")));
+        try {
+            assertEquals("Contacts:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[4]")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[4]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Contacts:".equals(driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[4]")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[4]"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("CV:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[5]")).getText());
+        assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[5]")));
+        try {
+            assertEquals("CV:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[5]")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[5]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("CV:".equals(driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[5]")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[5]"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Skills:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[6]")).getText());
+        assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[6]")));
+        try {
+            assertEquals("Skills:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[6]")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[6]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Skills:".equals(driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[6]")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[6]"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Social Media Link:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[7]")).getText());
+        assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[7]")));
+        try {
+            assertEquals("Social Media Link:", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[7]")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[7]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Social Media Link:".equals(driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[7]")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[7]"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Professional Experience", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[8]")).getText());
+        assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[8]")));
+        try {
+            assertEquals("Professional Experience", driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[8]")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[8]")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Professional Experience".equals(driver.findElement(By.xpath("//div[@id='description_wrapper']/h3[8]")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//div[@id='description_wrapper']/h3[8]"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertTrue(isElementPresent(By.id("image")));
+        try {
+            assertTrue(isElementPresent(By.id("image")));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.id("image"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
     }
 
     @After
