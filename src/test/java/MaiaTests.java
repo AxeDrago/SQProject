801448f9ac2d1d16bbbff7751978fb6377f6eec8
Ruben <maia.ruben@gmail.com>
2016-06-01 19:39:05 +0100

    Tests for Headers and correct errors in tests

--- src/test/java/MaiaTests.java
+++ src/test/java/MaiaTests.java
@@ -32,8 +32,8 @@ public class MaiaTests{
     @Before
     public void setUp() throws Exception {
         driver = new HtmlUnitDriver();
-        //baseUrl = "http://stagingserverqs.westeurope.cloudapp.azure.com/";
-        baseUrl = "http://127.0.0.1:8080";
+        baseUrl = "http://stagingserverqs.westeurope.cloudapp.azure.com/";
+        //baseUrl = "http://127.0.0.1:8080";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
